9fae76bc3114efbc677020d2437a00b05f072569
Miri Zulfugar <miri.zulfugar@gmail.com>
2020-08-22 20:56:50 +0400

    first commit

---
+++ database/migrations/2020_08_22_161809_create_channels_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateChannelsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('channels', function (Blueprint $table) {
+            $table->id();
+            $table->char('name', 255)->nullable();
+            $table->char('cover_image', 255)->nullable();
+            $table->integer('user_id')->nullable();
+            $table->enum('status', ['active', 'pending','banned'])->nullable();
+            $table->text('description')->nullable();
+            $table->text('body')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('channels');
+    }
+}
